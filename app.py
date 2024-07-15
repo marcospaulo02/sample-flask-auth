07fde2e9b778d1c0fce2ea621785d1274bfb7540
Marcos Silva <msilva@omniorthoandspine.com>
2024-07-15 16:56:59 -0400

    Monday 5pm Update

--- app.py
+++ app.py
@@ -1,13 +1,32 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from models.user import User
 from database import db
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "your_secret_key"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 
+login_manager = LoginManager()
 db.init_app(app)
+login_manager.init_app(app)
+
+#view login
 #Session <- conexao ativa
+@app.route('/login', methods=["POST"])
+def login():
+    data = request.json
+    username = data.get("username")
+    password = data.get("password")
+
+    if username and password:
+
+        user = User.query.filter_by(username=username).first()
+        if user:
+            if user.password == password:
+                return jsonify({"message": "Autenticao realizada com sucesso"}), 200
+    return jsonify({"message": "Credenciais invalidas"}), 400
+
 
 @app.route("/hello-world", methods=["GET"])
 def hello_world():
