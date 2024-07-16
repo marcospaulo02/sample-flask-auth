e2200ed9b8900e9c0918abe903f6c5a119124a99
marcosiMacHome <marcospaulo02@gmail.com>
2024-07-16 07:52:55 -0400

    Tuesday morning

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from models.user import User
 from database import db
-from flask_login import LoginManager
+from flask_login import LoginManager, login_user, current_user, logout_user, login_required
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "your_secret_key"
@@ -10,9 +10,13 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 login_manager = LoginManager()
 db.init_app(app)
 login_manager.init_app(app)
+login_manager.login_view = 'login'
 
 #view login
 #Session <- conexao ativa
+def load_user(user_id):
+    return User.query.get(user_id)
+
 @app.route('/login', methods=["POST"])
 def login():
     data = request.json
@@ -22,10 +26,18 @@ def login():
     if username and password:
 
         user = User.query.filter_by(username=username).first()
-        if user:
-            if user.password == password:
+        if user and user.password == password:
+                login_user(user)
+                print(current_user.is_authenticated)
                 return jsonify({"message": "Autenticao realizada com sucesso"}), 200
     return jsonify({"message": "Credenciais invalidas"}), 400
+
+
+@app.route('/logout', methods=['GET'])
+@login_required
+def logout():
+     logout_user()
+     return jsonify({"message": "Logout realizado com sucesso!"})
 
 
 @app.route("/hello-world", methods=["GET"])
